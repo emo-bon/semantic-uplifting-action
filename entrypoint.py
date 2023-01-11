eea27f29bd076a9fccafab2c2e52b5f26af3d3b3
cedricdecruw <30471340+cedricdcc@users.noreply.github.com>
2023-01-11 09:55:26 +0100

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,21 +1,13 @@
 import os
-from github import Github
 
 def main():
     # Get the GitHub access token from the environment
     git_access_token = os.getenv("GITHUB_TOKEN")
     git_repo = os.getenv("GITHUB_REPOSITORY")
 
-    # Use the access token to create a GitHub client
-    g = Github(git_access_token)
-
-    # Get the repository
-    repo = g.get_repo(git_repo)
-
-    print("Getting the latest release...")
-    print("")
-    print("Latest release: {}".format(repo.get_latest_release().title))
-    print("")
+    # Print the token
+    print("GitHub access token: " + git_access_token)
+    print("GitHub repository: " + git_repo)
     print("End of script.")
 
 if __name__ == "__main__":
