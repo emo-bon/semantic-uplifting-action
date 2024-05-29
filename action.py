fcd03efdd37298e5fade36eb69b431009d971b08
Bram Ulrichts <108867473+bulricht@users.noreply.github.com>
2024-05-29 16:28:41 +0200

    introduce splicing of sembench config

--- action.py
+++ action.py
@@ -1,21 +1,54 @@
 import json
+import yaml
 from pathlib import Path
 from pysembench import Sembench
 
-if __name__ == "__main__":
-    GITHUB_WORKSPACE = Path("/github/workspace")
-    SEMBENCH_KWARGS = Path("~sembench_kwargs.json")
+GITHUB_WORKSPACE = Path("/github/workspace")
+SEMBENCH_KWARGS = Path("~sembench_kwargs.json")
 
-    with open(GITHUB_WORKSPACE / SEMBENCH_KWARGS) as f:
-        kwargs = json.load(f)
+with open(GITHUB_WORKSPACE / SEMBENCH_KWARGS) as f:
+    kwargs = json.load(f)
 
-    sb = Sembench(
-        locations={
-            "home": kwargs["SEMBENCH_DATA_LOCATION"],
-            "input": kwargs["INPUT_DATA_LOCATION"],
-        },
-        sembench_config_path = kwargs["SEMBENCH_CONFIG_PATH"],
-        fail_fast=True,
-    )
+INPUT_DATA_LOCATION = kwargs["INPUT_DATA_LOCATION"]
+SEMBENCH_DATA_LOCATION = kwargs["SEMBENCH_DATA_LOCATION"]
+SEMBENCH_CONFIG_PATH = kwargs["SEMBENCH_CONFIG_PATH"]
 
-    sb.process()
+def splice_sembench_config(ignore_task_prefix):
+    """This function is a hack to circumvent the problem that a YAML file can't be parsed without resolving the !tags.
+    """
+    with open(SEMBENCH_CONFIG_PATH, "r") as f:
+        config = f.readlines()
+
+    spliced_config = []
+    skipping = False
+    for line in config:
+        if line.startswith(ignore_task_prefix):
+            skipping = True
+        elif line.startswith(" ") and skipping:
+            pass
+        elif line.startswith(" ") and not skipping:
+            spliced_config.append(line)
+        else:
+            skipping = False
+            spliced_config.append(line)
+
+    with open(SEMBENCH_CONFIG_PATH, "w") as f:
+        f.writelines(spliced_config)
+
+wp = yaml.load(open(GITHUB_WORKSPACE / "config/workflow_properties.yml"), Loader=yaml.BaseLoader)
+
+if wp["water"] == "nan":
+    splice_sembench_config("water")
+if wp["sediment"] == "nan":
+    splice_sembench_config("sediment")
+
+sb = Sembench(
+    locations={
+        "home": SEMBENCH_DATA_LOCATION,
+        "input": INPUT_DATA_LOCATION,
+    },
+    sembench_config_path = SEMBENCH_CONFIG_PATH,
+    fail_fast=True,
+)
+
+sb.process()
