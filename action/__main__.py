4b02050b7f165026dce3c0cec1f648f1c08ee82f
bulricht <108867473+bulricht@users.noreply.github.com>
2025-11-25 16:46:18 +0100

    refactor

--- action/__main__.py
+++ action/__main__.py
@@ -3,11 +3,11 @@ import glob
 import os
 import requests
 import shutil
-import yaml
 import zipfile
 from dotenv import load_dotenv
 from pathlib import Path
-from rocrate.rocrate import ROCrate, ContextEntity
+from rdflib import Graph
+from rocrate.rocrate import ROCrate
 from sema.bench import Sembench
 
 parser = argparse.ArgumentParser()
@@ -15,90 +15,76 @@ parser.add_argument('--dev', action='store_true')
 args = parser.parse_args()
 
 if args.dev:
+    assert Path(".env").exists(), ".env file is missing"
     load_dotenv(override=True)
 
-GITHUB_WORKSPACE = Path("/github/workspace")
-SEMBENCH_WORKSPACE = GITHUB_WORKSPACE / "~sembench_data_cache"
-PROFILE = os.getenv("PROFILE")
 
-def instantiate_rocrate():
-    crate = ROCrate()
-    context_entity = crate.add(
-        ContextEntity(
-            crate,
-            identifier=PROFILE,
-            properties={
-                "@type": "CreativeWork",
-                "name": "<name>",
-                "version": "<version>"
-            },
-        )
-    )
-    crate.root_dataset["conformsTo"] = context_entity
-    crate.write(GITHUB_WORKSPACE)
+GITHUB_WORKSPACE = Path(os.getenv("GITHUB_WORKSPACE"))
+SEMA_WORKSPACE = GITHUB_WORKSPACE / "sema-workspace"
+ROCRATE_PROFILE_URI = os.getenv("ROCRATE_PROFILE_URI")
+WATER_LOGSHEET_URL = os.getenv("WATER_LOGSHEET_URL")
+SEDIMENT_LOGSHEET_URL = os.getenv("SEDIMENT_LOGSHEET_URL")
+HARD_LOGSHEET_URL = os.getenv("HARD_LOGSHEET_URL")
+RDF_AGGREGATOR_GLOB = os.getenv("RDF_AGGREGATOR_GLOB")
 
 
 def clone_profile_crate_repo():
-    data_crate = ROCrate(GITHUB_WORKSPACE)
-    profile_uri = data_crate.root_dataset["conformsTo"]["@id"]
-    profile_crate = ROCrate()
-    profile_crate_metadata = requests.get(f"{profile_uri}/ro-crate-metadata.json").json()
-    for i in profile_crate_metadata.get("@graph", []):
-        profile_crate.add_or_update_jsonld(i)
-    zipball = requests.get(profile_crate.root_dataset["downloadUrl"])
-    with open("zipball.zip", "wb") as f:
+    profile_crate_metadata = requests.get(f"{ROCRATE_PROFILE_URI}/ro-crate-metadata.json").json()
+    download_url = None
+    for node in profile_crate_metadata.get("@graph", [{}]):
+        if node.get("@id", "") == "./":
+            download_url = node.get("downloadUrl")
+    assert download_url
+    zipball = requests.get(download_url)
+    with open(SEMA_WORKSPACE / "zipball.zip", "wb") as f:
         f.write(zipball.content)
-    with zipfile.ZipFile("zipball.zip", 'r') as f:
-        f.extractall("zipball")
-    for path in glob.glob("zipball/*/*"):
-        shutil.move(path, SEMBENCH_WORKSPACE)
-    os.remove(GITHUB_WORKSPACE / "zipball.zip")
-    shutil.rmtree(GITHUB_WORKSPACE / "zipball")
+    with zipfile.ZipFile(SEMA_WORKSPACE / "zipball.zip", 'r') as f:
+        f.extractall(SEMA_WORKSPACE / "zipball")
+    for path in glob.glob(str(SEMA_WORKSPACE / "zipball" / "*" / "*")):
+        shutil.move(path, SEMA_WORKSPACE)
+    os.remove(SEMA_WORKSPACE / "zipball.zip")
+    shutil.rmtree(SEMA_WORKSPACE / "zipball")
 
-def splice_sembench_config(ignore_task_prefix):
-    """This function is a hack to circumvent the problem that a YAML file can't be parsed without resolving the !tags.
-    """
-    with open(SEMBENCH_WORKSPACE / "sembench.yaml", "r") as f:
-        config = f.readlines()
 
-    spliced_config = []
-    skipping = False
-    for line in config:
-        if line.startswith(ignore_task_prefix):
-            skipping = True
-        elif line.startswith(" ") and skipping:
-            pass
-        elif line.startswith(" ") and not skipping:
-            spliced_config.append(line)
-        else:
-            skipping = False
-            spliced_config.append(line)
+class Aggregator:
+    def __init__(self):
+        self.globs = {k.strip(): v.strip() for k, v in (i.strip().split(":") for i in RDF_AGGREGATOR_GLOB.split(","))}
+        self.graph = Graph()
 
-    with open(SEMBENCH_WORKSPACE / "sembench.yaml", "w") as f:
-        f.writelines(spliced_config)
+    def aggregate(self):
+        for glb, fmt in self.globs.items():
+            for p in GITHUB_WORKSPACE.rglob(glb):
+                if p.is_file():
+                    try:
+                        self.graph.parse(p, format=fmt)
+                    except Exception as e:
+                        print(f"failed to parse {p}: {e}")
+
+        self.graph.serialize(GITHUB_WORKSPACE / "all-triples.ttl", format="ttl")
+
 
 if __name__ == "__main__":
     if not (GITHUB_WORKSPACE / "ro-crate-metadata.json").exists():
-        instantiate_rocrate()
+        crate = ROCrate()
+        crate.write(GITHUB_WORKSPACE)
 
-    if not SEMBENCH_WORKSPACE.exists(): 
-        SEMBENCH_WORKSPACE.mkdir(parents=True, exist_ok=True)
+    if not SEMA_WORKSPACE.exists(): 
+        SEMA_WORKSPACE.mkdir(parents=True, exist_ok=True)
+
+    if not any(SEMA_WORKSPACE.iterdir()):
         clone_profile_crate_repo()
+    
+    for habitat in ("water", "sediment", "hard"): # TODO introduce "common" habitat
+        if habitat == "common" or (os.getenv(f"{habitat.upper()}_LOGSHEET_URL")):
+            sb = Sembench(
+                locations={
+                    "observatory-profile": str(SEMA_WORKSPACE),
+                    "observatory-crate": str(GITHUB_WORKSPACE),
+                },
+                sembench_config_path = str(SEMA_WORKSPACE / f"sema_{habitat}.yaml"),
+                fail_fast=True,
+            )
 
-    wp = yaml.load(open(GITHUB_WORKSPACE / "config/workflow_properties.yml"), Loader=yaml.BaseLoader)
+            sb.process()
 
-    if wp["water"] == "nan":
-        splice_sembench_config("water")
-    if wp["sediment"] == "nan":
-        splice_sembench_config("sediment")
-
-    sb = Sembench(
-        locations={
-            "home": str(SEMBENCH_WORKSPACE),
-            "input": str(GITHUB_WORKSPACE),
-        },
-        sembench_config_path = str(SEMBENCH_WORKSPACE / "sembench.yaml"),
-        fail_fast=True,
-    )
-
-    sb.process()
+    Aggregator().aggregate()
