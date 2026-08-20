82e868cb5c7838addce6656f6c3bf3ded7f739be
cedricdcc <cedricdecruw@gmail.com>
2026-08-20 12:10:05 +0200

    feat: add action entry point to orchestrate Sembench, RDF aggregation, and RO-Crate generation

--- action/__main__.py
+++ action/__main__.py
@@ -1,7 +1,6 @@
 import argparse
 import logging
 import os
-import tempfile
 from dotenv import load_dotenv
 from pathlib import Path
 from sema.bench import Sembench
@@ -25,9 +24,7 @@ if args.dev:
 
 
 GITHUB_WORKSPACE = Path(os.getenv("GITHUB_WORKSPACE", "."))
-SEMA_WORKSPACE = Path(
-    os.getenv("SEMA_WORKSPACE", Path(tempfile.gettempdir()) / "sema-workspace")
-)
+SEMA_WORKSPACE = GITHUB_WORKSPACE / "sema-workspace"
 ROCRATE_PROFILE_URI = os.getenv("ROCRATE_PROFILE_URI")
 WATER_LOGSHEET_URL = os.getenv("WATER_LOGSHEET_URL")
 SEDIMENT_LOGSHEET_URL = os.getenv("SEDIMENT_LOGSHEET_URL")
